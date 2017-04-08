71c63577f813939c0989e34c33aced19afd74697
eros902002 <eros902002@googlemail.com>
2017-04-08 11:00:56 +0200

    Added coordinator layout to base activity

--- app/src/main/java/com/erostech/digga/ui/activities/MainNavigationActivity.java
+++ app/src/main/java/com/erostech/digga/ui/activities/MainNavigationActivity.java
@@ -4,14 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.erostech.digga.R;
 
 public class MainNavigationActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,13 +19,10 @@ public class MainNavigationActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -39,7 +35,9 @@ public class MainNavigationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_navigation);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
---
+++ app/src/main/java/com/erostech/digga/ui/fragments/ListingFragment.java
@@ -0,0 +1,10 @@
+package com.erostech.digga.ui.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by erosgarciaponte on 8/4/17.
+ */
+
+public class ListingFragment extends Fragment {
+}
